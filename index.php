2e8ed640e0e772d4fe7b5c7973a5fcc4e8c8ae4e
gbkim <rlarlqja1001@gmail.com>
2021-04-05 14:45:55 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+        <meta charset="utf-8">
+        <title>PHP Application</title>
+        <link href="css/site.css" rel="stylesheet">
+</head>
+
+<body>
+<div class="main-container">
+        <div class="cloud-image">
+        </div>
+         <img src="img/mantech-logo.svg" alt="mantech-logo" />
+         <div class="content-body">
+         <a href="https://accordions.bitbucket.io">
+            <img src="img/login_logo.svg" alt="login_logo" />
+         </a>
+                <div class="success-text">Success!</div>
+                  <div class="description line-1">
+                                <?php
+                                echo "build";
+                                ?>
+                                </div>
+                 <div class="description line-1">
+                                <?php
+                                $appType = "PHP";
+                                echo " $appType test app ";
+                                ?>
+                                </div>
+                <div class="content">
+        <div class="tweet-container">
+        </div>
+         <div class="next-steps-container">
+                    <div class="next-steps-header"></div>
+                    <div class="next-steps-body">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
